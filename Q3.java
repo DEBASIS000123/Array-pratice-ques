c2dd67876e6f8156c2fff165abe57a3a61a99642
DEBASIS MISHRA <128286377+DEBASIS000123@users.noreply.github.com>
2023-04-12 22:36:27 +0530

    Add files via upload

--- Q3.java
+++ Q3.java
@@ -1,19 +1,33 @@
+package Array_2D;
 import java.util.Scanner;
 public class Q3 {
-
+	//Sum of element in each row 1st type
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		int arr[]=new int [101];
-		int n;
-		System.out.println("Enter the integers between 1 to 100");
-		do{
-			n=sc.nextInt();
-			arr[n]++;
-		}while(n!=0);
-		for(int i=0;i<100;i++) {
-			if(arr[i]>0)
-				System.out.println(i+" occurs "+arr[i]+((arr[i]>1)?" times. ":" time. "));
+		Scanner  sc =new Scanner(System.in);
+		System.out.println("Enter the rows and columns :");
+		int r=sc.nextInt();
+		int c=sc.nextInt();
+		int arr[][]=new int [r][c];
+		System.out.println("Input the array elements :");
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				arr[i][j]=sc.nextInt();
+				
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			int sum=0;
+			for(int j=0;j<arr[i].length;j++) {
+				sum+=arr[i][j];
+			}
+			System.out.println("Sum of row "+i+" = "+sum);
+		}
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr[0].length;j++) {
+				System.out.print(arr[i][j]+"\t");
+			}
+			System.out.println();
 		}
 	}
 
--- Q5.java
+++ Q5.java
@@ -1,32 +1,27 @@
+package Array_2D;
 import java.util.Scanner;
 public class Q5 {
-	public static void max(int a[]) {
-		int max=a[0];
-		
-		for(int i=1;i<a.length;i++) {
-			if (a[i]>max) 
-				 max=a[i];
-		}
-		System.out.println("The largest number present in array is :"+max);
-	}
+//print major diagonal elements of a square matrix
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the size of array:");
-		int n=sc.nextInt();
-		int arr[]=new int[n];
-		System.out.println("Enter the array elements :");
-		for(int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
+		System.out.println("Enter the size of row and column :");
+		int n =sc.nextInt();
+		int arr[][]=new int[n][n];
+		System.out.println("Enter array elements :");
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				arr[i][j]=sc.nextInt();
+			}
 		}
-		
-		System.out.println("The array element are :");
-		for(int j:arr) {
-			System.out.print(j+" ");
+		System.out.println("The major diagonal numbers are :");
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if(i==j)
+					System.out.println(arr[i][j]+" ");
+			}
 		}
-		System.out.println();
-		max(arr);
+	
 	}
 
 }
-
